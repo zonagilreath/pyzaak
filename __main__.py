972a7333bb776b7e817d63d57015bdda6fd589d9
Zona <bryson.gilreath@gmail.com>
2018-05-09 18:41:33 -0500

    fully functioning, needs to be organized

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 import random
 from collections import defaultdict
 import operator
-# blah
+import time
+
 ops = { "+": operator.add, "-": operator.sub }
 
 class Card:
@@ -56,12 +57,20 @@ class Player:
 		self.name = name
 		self.score = 0
 		self.total = 0
+		self.standing = False
+		self.bust = False
+
+	def __repr__(self):
+		return(f"Name: {self.name}, Score: {self.score},\n"
+			f"Total: {self.total}, Standing: {self.standing},\n"
+			f"Bust: {self.bust}")
 
 	def get_side_deck(self):
-
+		space()
+		pause()
 		self.side = SideDeck(self.name)
 
-		print(f"Choose your hand")
+		print(f"{self.name}, choose your hand (0/4)")
 
 		for i in range(4):
 			print(f"\n{self.side.name}:")
@@ -72,14 +81,42 @@ class Player:
 			# print(list(enumerate(self.side.cards)))
 			choice = int(input(f"\nChoose by number: "))
 			self.side.choose_card(choice)
-			print(f"\n\n\n{self.name}'s hand ({i+1}/4): \n"
+			space()
+			print(f"{self.name}'s hand ({i+1}/4): \n"
 				f"{self.side.hand}")
 
+	def round_reset(self):
+		self.total = 0
+		self.bust = False
+		self.standing = False
+
+	def check_bust(self):
+		if self.total > 20:
+			self.bust = True
+			self.total = -220
+			print(self.name,"busts!")
 
 
+def pause(sec=0.4):
+	time.sleep(sec)
+
+def space(size=20):
+	for i in range(size):
+		print("\n")
+
+def continue_play(player):
+	if not(player.bust or player.standing):
+		return True
+	return False
+
+def stand(player):
+	player.standing = True
+
+def pass_turn(player):
+	pass
 
 
-def draw_phase(player):
+def draw_phase(player, maindeck):
 	print(f"{player.name} draws...\n")
 	draw = maindeck.draw_card()
 	print(draw.value)
@@ -88,48 +125,61 @@ def draw_phase(player):
 
 
 def play_phase(player):
-	played = False
-	while played == False:
-		print("1: Play card    2: Hit    3: Stay")
-		action = input("Choose an action: ")
-		action = action.strip()
-		if action not in ["1", "2", "3"]:
+	actions = {"1": play_card,
+				"2": pass_turn,
+				"3": stand
+				}
+	show_hand(player)
+	space(size=4)
+	while True:
+		print("Choose an action: ")
+		print("1: Play card    2: Pass    3: Stand")
+		
+		try:
+			actions[input("Selection: ")](player)
+			break
+		except KeyError:
 			print("I'm sorry, I didn't understand that.\n"
-				"Please enter 1, 2, or 3. ")
-		if action == "1":
-			played = True
-			return "card"
-		if action == "2":
-			played = True
-			return "hit"
-		if action == "3":
-			played = True
-			return "stay"
+				"Please enter 1, 2, or 3.")
 
-def empty_hand(player):
-	played = False
-	while played == False:
-		print("1: Hit    2: Stay")
-		action = input("Choose an action: ")
-		action = action.strip()
-		if action not in ["1", "2"]:
+
+def play_phase_no_card(player):
+	actions = {"1": pass_turn,
+				"2": stand
+				}
+	while True:
+		print("Choose an action: ")
+		print("1: Pass    2: Stand")
+		
+		try:
+			actions[input("Selection: ")](player)
+			break
+		except KeyError:
 			print("I'm sorry, I didn't understand that.\n"
 				"Please enter 1 or 2.")
-		if action == "1":
-			played = True
-			return "hit"
-		if action == "2":
-			played = True
-			return "stay"
 
+
+def show_hand(player):
+	display = ""
+	for i in player.side.hand:
+		display += f"{i}   "
+	print(f"{player.name}'s hand: ", display)
+
+
+def show_hand_numbered(player):
+	print(f"{player.name}'s hand:\n")
+	for i in range(1, len(player.side.hand)+1):
+		print(f"{i}: {player.side.hand[i-1]}")
+
+def apply_card(player, choice):
+	player.total = ops[choice.valence](player.total,choice.value)
+	print(f"\n{player.name}'s new total is {player.total}\n")
 
 
 def play_card(player):
 	while True:
-		print(f"{player.name}'s hand:\n")
-
-		for i in range(1, len(player.side.hand)+1):
-			print(f"{i}: {player.side.hand[i-1]}")
+		
+		show_hand_numbered(player)
 
 		try:
 			choice = player.side.hand.pop(int(input("Please enter a number: "))-1)
@@ -137,30 +187,119 @@ def play_card(player):
 		except (ValueError, IndexError):
 			print("I'm sorry, I didn't understand that.\n"
 				"Please enter a valid number.\n")
-	player.total = ops[choice.valence](player.total,choice.value)
-	print(f"\n{player.name}'s new total is {player.total}\n")
+	apply_card(player, choice)
+	play_phase_no_card(player)
 
 
 
-def round(player1, player2):
+
+
+
+def player_turn(player, maindeck):
+	space()
+	pause()
+	if continue_play(player):
+		draw_phase(player, maindeck)
+	if continue_play(player):
+		play_phase(player) if player.side.hand else play_phase_no_card(player)
+		player.check_bust()
+
+
+
+def play_order(player1, player2):
 	players = [player1, player2]
 	first = players.pop(random.randint(0,1))
 	second = players[0]
 	print(f"{first.name} goes first!")
+	return first, second
+
+
+def win_round(player1, player2):
+	if player1.total == player2.total:
+		print("No winner!")
+	else:
+		winner = max(player1, player2, key=operator.attrgetter('total'))
+		print(f"{winner.name} wins the round!")
+		winner.score += 1
 
 
 
+def round(player1, player2, maindeck):
+	player1.round_reset()
+	player2.round_reset()
+	first, second = play_order(player1, player2)
+	while (continue_play(first) or continue_play(second)):
+		player_turn(first, maindeck)
+		player_turn(second, maindeck)
+	space(size=4)
+	win_round(player1, player2)
+	pause(sec=2.5)
+
+def score_to_win():
+	while True:
+
+		try:
+			score_to_win = int(input("How many points to win?  ").strip())
+			break
+		except TypeError:
+			print("I'm sorry, I didn't understand that.\n"
+				"Please enter a number.\n")
+	return score_to_win
+
+def get_players():
+	player1 = Player(input("Player 1 name: "))
+	pause()
+	print(f"Hi, {player1.name}!")
+	space(size=4)
+	pause()
+	player2 = Player(input("Player 2 name: "))
+	pause()
+	print(f"Hey, {player2.name}!")
+	space(size=4)
+	return player1, player2
+
+
+
+def game(player1, player2, score_to_win, maindeck):
+	player1.get_side_deck()
+	player2.get_side_deck()
+	while (player1.score < score_to_win) and (player2.score < score_to_win):
+		round_count = 1
+		print("Round", round_count)
+		round(player1, player2, maindeck)
+		round_count += 1
+	winner = max(player1, player2, key=operator.attrgetter('score'))
+	loser = min(player1, player2, key=operator.attrgetter('score'))
+	print(f"{winner.name} wins the game!!!")
+	pause()
+	print(f"Congrats {winner.name}!")
+	pause()
+	print(f"Better luck next time {loser.name}...")
+	pause(sec=4)
+
+
+
+def __main__():
+	space()
+	player1, player2 = get_players()
+	stw = score_to_win()
+	maindeck = MainDeck()
+	game(player1, player2, stw, maindeck)
 
 
 
 
 
 # testing
-player =  Player("player")
-maindeck = MainDeck()
+# player1 = Player("player1")
+# player2 = Player("player2")
+# maindeck = MainDeck()
 
 
-player.get_side_deck()
-draw_phase(player)
+# player1.get_side_deck()
+# show_hand(player1)
+# player2.get_side_deck()
 
-play_card(player)
+# round(player1, player2)
+
+__main__()
